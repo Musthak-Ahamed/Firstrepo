2707d21dda9ec82b53fc4b6465370180e3f65176
Musthak-Ahamed <138383334+Musthak-Ahamed@users.noreply.github.com>
2025-01-18 14:22:38 +0530

    Update SearchString.java

--- Program/Linear search/SearchString.java
+++ Program/Linear search/SearchString.java
@@ -1,4 +1,27 @@
 public class SearchString{
   public static void main(String[] args){
+    String name = "musthak";
+    char target = 's';
+    boolean ans = search(name,target);
+    if(ans == true){
+      System.out.println(target + "is present");
+    }
+    else{
+      System.out.println(target + "is not present");
+    }
   }
+  static boolean search(String name,char target){
+   if(name.length() == 0){ 
+     return false;
+   }
+
+    for(int i=0;i<name.length;i++){
+       if(target == name.charAt(i)){ 
+         return true;
+       }
+}
+    return false;
+}
+
+
 }
