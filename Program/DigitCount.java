4bb19404e3212c6f59b2577b4021a76076b58493
Musthak-Ahamed <138383334+Musthak-Ahamed@users.noreply.github.com>
2025-01-19 23:19:24 +0530

    Update DigitCount.java

--- Program/DigitCount.java
+++ Program/DigitCount.java
@@ -1,1 +1,10 @@
-
+import java.util.*;
+public class DigitCount{
+  public static void main(String[] args){
+    int num = 1784;
+    String digit = Integer.toString(num);
+    System.out.println(digit);
+    int count = digit.length();
+    System.out.println(count);
+  }
+}
